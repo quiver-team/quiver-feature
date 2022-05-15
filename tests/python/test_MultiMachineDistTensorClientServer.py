b0a9ab999fea5c294341bc8c43e8b9977da07c0a
eedalong <ee_dalong@163.com>
2022-05-15 04:09:03 +0000

    add client server test

--- tests/python/test_MultiMachineDistTensorClientServer.py
+++ tests/python/test_MultiMachineDistTensorClientServer.py
@@ -5,10 +5,14 @@ import numpy as np
 import time
 import threading
 from typing import List
-import config
-import torch.multiprocessing as mp
-from quiver_feature import DistHelper
 import qvf
+import config
+import quiver
+import torch.multiprocessing as mp
+from quiver_feature import TensorEndPoint, Range
+from quiver_feature import DistHelper
+#from tmp import DistTensor as DistTensorPGAS
+from quiver_feature import DistTensorPGAS, LocalTensorPGAS
 
 MASTER_IP = "155.198.152.17"
 HLPER_PORT = 5678
@@ -21,44 +25,41 @@ SAMPLE_SIZE = 80000
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('-server_rank', type=int, help='server_rank')
 parser.add_argument('-device_per_node', type=int, help="how many process per server")
-parser.add_argument('-server_world_size', type=int, default = 2, help="world size")
 
 args = parser.parse_args()
 
 
-def client_process(rank):
+def feature_process(rank, server_rank, tensor_endpoints, local_tensor_pgas, cached_range, whole_tensor, SERVER_WORLD_SIZE, NUM_ELEMENT, SAMPLE_SIZE, FEATURE_DIM):
+
+    torch.cuda.set_device(rank)
+    host_indice = np.random.randint(NUM_ELEMENT * (1 - server_rank), high= (SERVER_WORLD_SIZE - server_rank) * NUM_ELEMENT - 1, size=(SAMPLE_SIZE, ))
+    indices = torch.from_numpy(host_indice).type(torch.long)
+    indices_device = indices.to(rank)
 
     pipe_param = qvf.PipeParam(config.QP_NUM, config.CQ_MOD, config.CTX_POLL_BATCH, config.TX_DEPTH, config.POST_LIST_SIZE)
-    local_com_endpoint = qvf.ComEndPoint(0, config.MASTER_IP, config.PORT_NUMBER)
-    remote_com_endpoint = qvf.ComEndPoint(1, config.MASTER_IP, config.PORT_NUMBER)
-    dist_tensor_client = qvf.DistTensorClient(0, [local_com_endpoint, remote_com_endpoint], pipe_param)
-    registered_tensor = dist_tensor_client.create_registered_float32_tensor([config.SAMPLE_NUM, config.FEATURE_DIM])
-    print("Before Collect, Check RegisteredTensor Shape ", registered_tensor.shape)
-    local_idx = torch.arange(0, config.SAMPLE_NUM, dtype=torch.int64)
-    remote_idx = torch.randint(0, config.NODE_COUNT, (config.SAMPLE_NUM, ), dtype=torch.int64)
-
-    if config.TEST_TLB_OPTIMIZATION:
-        print("Using TLB Optimization")
-        remote_idx, _= torch.sort(remote_idx)
-
-    local_offsets  = local_idx * config.FEATURE_DIM * config.FEATURE_TYPE_SIZE
-    remote_offsets = remote_idx * config.FEATURE_DIM * config.FEATURE_TYPE_SIZE
+    dist_tensor = DistTensorPGAS(rank, server_rank, tensor_endpoints, pipe_param, [SAMPLE_SIZE, FEATURE_DIM], local_tensor_pgas, cached_range)
 
     # warm up
-    dist_tensor_client.sync_read(1, registered_tensor, local_offsets, remote_offsets)
-    #registered_tensor[:] = 0
+    data = dist_tensor[indices_device]
+    torch.cuda.synchronize()
+    TEST_COUNT = 1000
+    start = time.time()
+    consumed = 0
+    for i in range(TEST_COUNT):
 
-    start_time = time.time()
-    dist_tensor_client.sync_read(1, registered_tensor, local_offsets, remote_offsets)
-    consumed = time.time() - start_time
+        host_indice = np.random.randint(NUM_ELEMENT * (1 - server_rank), high= (SERVER_WORLD_SIZE - server_rank) * NUM_ELEMENT - 1, size=(SAMPLE_SIZE, ))
+        indices = torch.from_numpy(host_indice).type(torch.long)
+        if config.TEST_TLB_OPTIMIZATION:
+            indices, _ = torch.sort(indices)
 
-    print("Begin To Check Result...")
-    registered_tensor = registered_tensor.to('cpu')
-    for row in range(config.SAMPLE_NUM):
-        if not all(registered_tensor[row] == remote_idx[row]):
-            print(f"Result Check Failed At {row}, Expected {remote_idx[row]}, But got {registered_tensor[row]}, Local Offsets {local_offsets[row]}, Remote Offsets {remote_offsets[row]}")
-            exit()
-    print(f"Result Check Passed!, Throughput = {registered_tensor.numel() * 4 / 1024 / 1024 / consumed} MB/s")
+        local_offsets =  torch.arange(0, SAMPLE_SIZE) * 4 * FEATURE_DIM
+        remote_offsets = (indices - NUM_ELEMENT) * 4
+
+        start = time.time()
+        dist_tensor.dist_tensor_client.sync_read(1 - server_rank, dist_tensor.registered_tensor, local_offsets, remote_offsets)
+        consumed += time.time() - start
+
+    print(f"Result Check Successed! Throughput = {data.numel() * 4 * TEST_COUNT/ 1024 / 1024 / consumed} MB/s")
 
 
 
@@ -66,21 +67,40 @@ def client_process(rank):
 if __name__ == "__main__":
 
 
+    SERVER_WORLD_SIZE = 2
+    START_SERVER = True
+    CACHE_RATIO = args.cache_ratio
+    LOCAL_SERVER_RANK = args.server_rank
+
+
+    cached_range = Range(0, int(CACHE_RATIO * NUM_ELEMENT * SERVER_WORLD_SIZE))
+    UNCACHED_NUM_ELEMENT = (NUM_ELEMENT * SERVER_WORLD_SIZE - cached_range.end) // SERVER_WORLD_SIZE
+
+
+    host_tensor = np.arange((UNCACHED_NUM_ELEMENT + cached_range.end ) * FEATURE_DIM)
+    host_tensor = host_tensor.reshape((UNCACHED_NUM_ELEMENT + cached_range.end), FEATURE_DIM)
+    tensor = torch.from_numpy(host_tensor).type(torch.float32)
+
+    # Build local_tensor_pgas
+    local_tensor_pgas = LocalTensorPGAS(0, device_list=list(range(args.device_per_node)), device_cache_size="8G", cache_policy="device_replicate")
+    local_tensor_pgas.from_cpu_tensor(tensor)
+
+    # Decide Range Information
+    range_list = []
+    for idx in range(SERVER_WORLD_SIZE):
+        range_item = Range(cached_range.end + UNCACHED_NUM_ELEMENT * idx, cached_range.end + UNCACHED_NUM_ELEMENT * (idx + 1))
+        range_list.append(range_item)
+
     # Exchange information with each other
-    dist_helper = DistHelper(MASTER_IP, HLPER_PORT, args.server_world_size, args.server_rank)
+    dist_helper = DistHelper(MASTER_IP, HLPER_PORT, SERVER_WORLD_SIZE, LOCAL_SERVER_RANK)
+    tensor_endpoints_list: List[TensorEndPoint] = dist_helper.exchange_tensor_endpoints_info(range_list[LOCAL_SERVER_RANK])
 
     # Start server thread
     def server_thread(dist_helper):
-        pipe_param = qvf.PipeParam(config.QP_NUM, config.CQ_MOD, config.CTX_POLL_BATCH, config.TX_DEPTH, config.POST_LIST_SIZE)
-        data = torch.empty((config.NODE_COUNT, config.FEATURE_DIM), dtype=torch.float)
-        for row in range(config.NODE_COUNT):
-            data[row] = row
-        dist_tensor_server = qvf.DistTensorServer(config.PORT_NUMBER, args.server_world_size + 1, config.QP_NUM)
-        dist_tensor_server.serve_tensor(data)
+        dist_tensor_server = qvf.DistTensorServer(config.PORT_NUMBER, SERVER_WORLD_SIZE * args.device_per_node, config.QP_NUM)
+        dist_tensor_server.serve_tensor(tensor)
         dist_helper.sync_start()
         dist_tensor_server.join()
-
-
     x = threading.Thread(target=server_thread, args=(dist_helper, ))
     x.daemon = True
     x.start()
@@ -88,7 +108,10 @@ if __name__ == "__main__":
     # Wait all servers start
     dist_helper.sync_end()
 
+    print(f"Check All TensorEndPoints {tensor_endpoints_list}")
+    whole_tensor = torch.cat([tensor[:cached_range.end, ]] + [tensor[cached_range.end:, ]] * SERVER_WORLD_SIZE)
 
-    mp.spawn(client_process, nprocs=args.device_per_node, join=True)
+
+    mp.spawn(feature_process, nprocs=args.device_per_node, args=(LOCAL_SERVER_RANK, tensor_endpoints_list, local_tensor_pgas, cached_range, whole_tensor, SERVER_WORLD_SIZE, NUM_ELEMENT, SAMPLE_SIZE, FEATURE_DIM), join=True)
 
     dist_helper.sync_all()
